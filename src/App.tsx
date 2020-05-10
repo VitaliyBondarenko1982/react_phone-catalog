da9fa9b4c6f4eb3fe781f8d572a77f6b250d74e8
Vitalii <vitalii.bondarenko.ua@gmail.com>
2020-05-10 13:37:17 +0300

    edit readme

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,8 @@ import React from 'react';
 
 import './App.css';
 
-import PhoneCatalog from './PhoneCatalog';
-import PhoneDetailsPage from './PhoneDetailsPage';
-
 const App = () => (
-  <div className="container-fluid">
-    <div className="row">
-      <div className="col-md-2">
-        <section>
-          <h2>Filter</h2>
-
-          <label>
-            <div>Search:</div>
-            <input />
-          </label>
-
-          <label>
-            <div>Sort by:</div>
-            <select>
-              <option value="name">Alphabetical</option>
-              <option value="age">Newest</option>
-            </select>
-          </label>
-        </section>
-
-        <section>
-          <h2>Shopping Cart</h2>
-          <ul>
-            <li>Phone 1</li>
-            <li>Phone 2</li>
-            <li>Phone 3</li>
-          </ul>
-        </section>
-      </div>
-
-      <div className="col-md-10">
-        <PhoneDetailsPage />
-        <PhoneCatalog />
-      </div>
-    </div>
-  </div>
+  <div>Phone Catalog</div>
 );
 
 export default App;
