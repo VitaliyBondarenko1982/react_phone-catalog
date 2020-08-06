71a472a737e7f16ff591745f4906c886a9193d09
Vitalii <vitalii.bondarenko.ua@gmail.com>
2020-08-06 11:23:25 +0300

    adaptive cart page

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 import cx from 'classnames';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
