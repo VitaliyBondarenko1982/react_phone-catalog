cfaa28d738be3305172d8e4724838be218dd9aa4
Vitalii <vitalii.bondarenko.ua@gmail.com>
2023-09-23 12:14:12 +0300

    Add slider to phone details page

--- src/pages/PhoneDetails/PhoneDetails.tsx
+++ src/pages/PhoneDetails/PhoneDetails.tsx
@@ -1,25 +1,36 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { getPhoneDetails } from '../../api';
-import { Loader, ProductDetails } from '../../components';
-import { IProductDetails } from '../../types';
+import { getPhoneDetails, getPhones } from '../../api';
+import { Loader, ProductDetails, ProductsSlider } from '../../components';
+import { IProduct, IProductDetails } from '../../types';
 
 const PhoneDetails = () => {
   const { slug } = useParams<{ slug: string }>();
   const [phone, setPhone] = useState<IProductDetails | null>(null);
+  const [phones, setPhones] = useState<IProduct[]>([]);
 
   useEffect(() => {
     if (!slug) return;
 
     getPhoneDetails(slug, setPhone);
+    getPhones(setPhones);
   }, [slug]);
+
+  const similarPhones = useMemo(() => {
+    return phones.slice(0, 12);
+  }, [phones]);
 
   if (!phone) {
     return <Loader />;
   }
 
-  return <ProductDetails product={phone} />;
+  return (
+    <>
+      <ProductDetails product={phone} />
+      <ProductsSlider products={similarPhones} title="You may also like" />
+    </>
+  );
 };
 
 export default PhoneDetails;
